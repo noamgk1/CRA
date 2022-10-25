4e52693663080d02a4cf60e8431218b2920cacd4
Noam Karmon <noamgk1@gmail.com>
2022-10-25 11:42:33 +0300

    CRA without CRA

---
+++ README.md
@@ -0,0 +1,23 @@
+# React App with webpack
+
+This is a simple React app with webpack.
+
+## How to run
+
+1. Install dependencies
+
+```bash
+npm install
+```
+
+2. Build webpack
+
+```bash
+npm run build
+```
+
+3. Run the app
+
+```bash
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div align="center">
+      <h1>React App</h1>
+      <h3>This is a React App created using the webpack.</h3>
+      <p>
+        React is a JavaScript library for building user interfaces. It is
+        maintained by Facebook and a community of individual developers and
+        companies.
+      </p>
+    </div>
+  );
+};
+
+export default App;
